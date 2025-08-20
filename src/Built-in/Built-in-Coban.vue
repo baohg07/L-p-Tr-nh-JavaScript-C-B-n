168e8590128c81d97663e0c16a70b89652437883
baohg07 <baohg82@gmail.com>
2025-08-20 22:34:16 +0700

    add Built-in

---
+++ src/Built-in/Built-in-Coban.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Built-in Components</h1>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+    
+    const message = ref('Test console.log')
+    console.log(message.value)
+</script>
