991a0a2fab4bedb0bf0a5afe19c8436c820ab4b3
baohg07 <baohg82@gmail.com>
2025-08-20 23:19:29 +0700

    add new Built-in

--- src/Built-in/Built-in-Coban.vue
+++ src/Built-in/Built-in-Coban.vue
@@ -8,37 +8,37 @@
     const name = ref('')
 
     // Demo Console
-    // const message = ref('Test console.log')
-    // console.log(message.value)
-    // console.warn('Test console.warn')
-    // console.error('Test console.error')
-    // console.info('Test console.info')
+    const message = ref('Test console.log')
+    console.log(message.value)
+    console.warn('Test console.warn')
+    console.error('Test console.error')
+    console.info('Test console.info')
 
     // Demo confirm
-    // confirm('Bạn có bằng lái xe chưa?') ? console.log('Có Bằng Rồi') : console.log('Chưa Có')
+    confirm('Bạn có bằng lái xe chưa?') ? console.log('Có Bằng Rồi') : console.log('Chưa Có')
 
     // Demo Prompt
-    // const input = prompt('Nhập tên của bạn')
-    // if (input) {
-    //     name.value = input
-    // } else {
-    //     name.value = 'Không có tên'
-    // }
-    // console.log('Tên của bạn là:', name.value)
+    const input = prompt('Nhập tên của bạn')
+    if (input) {
+        name.value = input
+    } else {
+        name.value = 'Không có tên'
+    }
+    console.log('Tên của bạn là:', name.value)
 
     // Demo setTimeout
-    // setTimeout(() => {
-    //     alert('Test setTimeout 5s')
-    //     console.log('Test setTimeout 5s')
-    // }, 2000)
+    setTimeout(() => {
+        alert('Test setTimeout 5s')
+        console.log('Test setTimeout 5s')
+    }, 2000)
 
     // Demo setInterval
-    // let count = 0
-    // const interval = setInterval(() => {
-    //     console.log('Interval lần thứ : ' + ++count)
-    //     if (count === 5) {
-    //         clearInterval(interval)
-    //         console.log('Đã dừng setInterval sau 5 lần')
-    //     }
-    // }, 1000)
+    let count = 0
+    const interval = setInterval(() => {
+        console.log('Interval lần thứ : ' + ++count)
+        if (count === 5) {
+            clearInterval(interval)
+            console.log('Đã dừng setInterval sau 5 lần')
+        }
+    }, 1000)
 </script>
